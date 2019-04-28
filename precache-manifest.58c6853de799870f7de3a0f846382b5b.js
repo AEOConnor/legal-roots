b580b7fdf7ebcb03fcf78c2a15709491909784a9
Alanna <oconnor.alannae@gmail.com>
2019-04-28 12:42:23 -0400

    Updates

---
+++ precache-manifest.58c6853de799870f7de3a0f846382b5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3953fa036511f8896290379f7503ec11",
+    "url": "/legal-roots/index.html"
+  },
+  {
+    "revision": "a30a1145ee110eeecaca",
+    "url": "/legal-roots/static/css/main.c66a1839.chunk.css"
+  },
+  {
+    "revision": "797407584c91a5c5262e",
+    "url": "/legal-roots/static/js/2.d5de9855.chunk.js"
+  },
+  {
+    "revision": "a30a1145ee110eeecaca",
+    "url": "/legal-roots/static/js/main.79cf55ee.chunk.js"
+  },
+  {
+    "revision": "8795550071f726bbb72c",
+    "url": "/legal-roots/static/js/runtime~main.e6c1132e.js"
+  },
+  {
+    "revision": "e0b1953a25bb8d6aa74d6e61eb22560f",
+    "url": "/legal-roots/static/media/aboutServices1.e0b1953a.jpg"
+  },
+  {
+    "revision": "deeb66d55b4d09b2249de2e86f74a0b5",
+    "url": "/legal-roots/static/media/aboutServices2.deeb66d5.jpg"
+  },
+  {
+    "revision": "4a7a6d0382b6c670cbd2d9fb5fd57545",
+    "url": "/legal-roots/static/media/aboutServices3.4a7a6d03.jpg"
+  },
+  {
+    "revision": "c00315c86fef13525e01de115c3e5bed",
+    "url": "/legal-roots/static/media/homeOffice.c00315c8.jpg"
+  },
+  {
+    "revision": "bfc719056a426115e0c27e6374a6b30a",
+    "url": "/legal-roots/static/media/legalRootsLogo.bfc71905.png"
+  }
+]);
