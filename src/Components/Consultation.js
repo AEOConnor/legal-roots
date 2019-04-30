1b095011f0d507d24ee4385eeb9961bf820ec929
Alanna <oconnor.alannae@gmail.com>
2019-04-30 11:34:58 -0400

    updates to all pages, including styling

--- src/Components/Consultation.js
+++ src/Components/Consultation.js
@@ -1,30 +1,41 @@
 import React from 'react';
-
+import consultation from '../assets/consultation.jpg';
 const Consultation = (props) => {
 
-    return (
+  return (
     <main>
-      <p>Contact Me</p>
-      <div>
-        <form action="/action_page.php" onSubmit={props.handleFormSubmit}>
-          <label>First Name</label>
-          <input type="text" id="fName" name="firstname" value={props.fName} onChange={props.handleChange} placeholder="Your name.." />
-          <label>Last Name</label>
-          <input type="text" id="lName" name="lastname" value={props.lName}  onChange={props.handleChange}placeholder="Your last name.." />
+      <h3>Contact us for an appointment for a 30-minute free consultation
+        </h3>
+      <div className="wrapper Consultation">
+        
+        <div className="consultationPhoto">
+          <img src={consultation} alt="Coffee cup."/>
+        </div>
+        <div className="consultationInfo">
+          <p>You may book a free consultation via telephone, email, or simply leave us a message below. If you wish to accept our free consultation, you would need to attend either a telephone conversation or an in-person meeting at our office.</p>
+          <div>
+            <form action="/action_page.php" onSubmit={props.handleFormSubmit}>
+              <label>First Name</label>
+              <input type="text" id="fName" name="firstname" value={props.fName} onChange={props.handleChange} placeholder="Your name.." />
+              <label>Last Name</label>
+              <input type="text" id="lName" name="lastname" value={props.lName} onChange={props.handleChange} placeholder="Your last name.." />
 
 
-          <label>Email</label>
-          <input type="email" id="email" name="email" value={props.email} onChange={props.handleChange} placeholder="Your email" />
+              <label>Email</label>
+              <input type="email" id="email" name="email" value={props.email} onChange={props.handleChange} placeholder="Your email" />
 
-          <label>Subject</label>
-          <textarea id="message" name="subject" valeue={props.message} onChange={props.handleChange}placeholder="Write something.."></textarea>
-          <input type="submit" value="Submit" />
-        </form>
+              <label>Subject</label>
+              <textarea id="message" name="subject" valeue={props.message} onChange={props.handleChange} placeholder="Write something.."></textarea>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </div>
       </div>
+
     </main >
   )
 
-  
+
 }
 
 export default Consultation;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import aboutServices2 from '../assets/aboutServices2.jpg';
 
 const Contact = () => {
   return (
     <main>
-      <h1>Contact</h1>
+      <div className="wrapper Contact">
+        <div className="contactInfo">
+          <h3>LegalRoots</h3>
+          <a href="https://www.google.com/maps/place/33+Bloor+St+E+5th+Floor,+Toronto,+ON+M4W+3H1/@43.6698664,-79.3877239,17z/data=!3m1!4b1!4m5!3m4!1s0x882b34addb89fcc5:0xe873839a2079abb5!8m2!3d43.6698625!4d-79.3855352" target="_blank">33 Bloor Street East, 5th Floor, Toronto, Ontario
+M4W3H1</a>
+        <p>Phone <a href="telto:647-890-2642">(647) 890 2642</a></p>
+        <p>Business hours: 8:30 AM to 5:00 PM </p>
+          <p>If you inquire specific information about our services and fees, please contact us at  <a href="mailto:info@legalroots.ca">info@legalroots.ca</a> </p>
+   
+
+
+
+
+
+        </div>
+        <div className="contactPhoto">
+          <img src={aboutServices2} alt=""/>
+        </div>
+      </div>
     </main>
   )
 }
